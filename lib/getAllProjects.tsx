7f92ae26de65862c88cc4d6359136fc601e02105
Kyle Huynh <kyle_huynh@hotmail.com>
2023-08-21 09:49:25 -0700

    quick fix getAllProjects

--- lib/getAllProjects.tsx
+++ lib/getAllProjects.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
 export default async function getAllProjects() {
-    const res = await fetch('https://jsonplaceholder.typicode.com/albums' {
-        
-    })
-    if (!res.ok) throw new Error('Failed to fetch data')
+    // const res = await fetch('https://jsonplaceholder.typicode.com/albums' {
+    //     headers: {
+    //         Authorization
+    //     }
+    // })
+    // if (!res.ok) throw new Error('Failed to fetch data')
 
-    return res.json()
+    // return res.json()
+    return 'null'
 }
