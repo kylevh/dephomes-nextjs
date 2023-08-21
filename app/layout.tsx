f328a372198baf36c77e98e8830220d99a7bbfa6
Kyle Huynh <kyle_huynh@hotmail.com>
2023-08-21 09:45:14 -0700

    remove useclient in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
-'use client'
 import Header from '@/components/Header'
 import './globals.css'
 import type { Metadata } from 'next'
-import { motion, AnimatePresence } from 'framer-motion'
-import { usePathname } from 'next/navigation'
 
 export const metadata: Metadata = {
   title: 'DEPHomes',
@@ -16,16 +13,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
 
-  const pageKey = usePathname()
 
   return (
     <html lang="en">
       <body>
         <Header />
-        {/* initial={{ opacity: 0, scale: 1.2 }} animate={{ opacity: 1, scale: 1 }} exit={{ opacity: 0, scale: 1.2 }} transition={{ type: "tween", duration: .5 }} */}
-        <AnimatePresence mode="wait">
           {children}
-        </AnimatePresence>
       </body>
     </html>
   )
